42b74200b89e219882e4ce35f7982392c07536e8
Nishant Sirohi <68343049+nishantsirohi23@users.noreply.github.com>
2025-08-11 03:00:52 +0530

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Iconify Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Iconify Demo Home Page'),
     );
   }
 }
